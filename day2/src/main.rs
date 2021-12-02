6176e311041fb267c8a1746b599df05d9734f9d7
rubensandstrom@gmail.com <rubensandstrom@gmail.com>
2021-12-02 14:06:15 +0100

    clean

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,62 +1,93 @@
 use std::fs;
 
 
-// Super messy!
+struct Sub {
+    horizontal_pos: i32,
+    depth: i32,
+    aim: i32
+}
+
+impl Sub {
+    fn new() -> Self {
+        Self {
+            horizontal_pos: 0,
+            depth: 0,
+            aim: 0,
+        }
+    }
+
+    fn part1 (&mut self, input: (&str, i32)) {
+        match input {
+            ("forward", val) => {
+                self.horizontal_pos += val;
+            }
+            ("up", val) => {
+                self.depth -= val;
+            }
+            ("down", val) => {
+                self.depth += val;
+            }
+            _ => panic!("panic in part1")
+        }
+    }
+
+    fn part2 (&mut self, input: (&str, i32)) {
+        match input {
+            ("forward", val) => {
+                self.horizontal_pos += val;
+                self.depth += self.aim * val;
+            }
+            ("up", val) => {
+                self.aim -= val;
+            }
+            ("down", val) => {
+                self.aim += val;
+            }
+            _ => panic!("panic in part1")
+        }
+    }
+}
+
+fn format_input(input: &str) -> Vec<(&str, i32)> {
+
+    let mut format_input: Vec<(&str, i32)> = vec!();
+    for lines in input.trim().lines() {
+        let mut parts = lines.split(' ');
+        let cmd = parts
+            .next()
+            .expect("No string here");
+        let val: i32 = parts
+            .next()
+            .expect("No value here")
+            .parse()
+            .expect("Not an integer");
+        format_input.push((cmd, val));
+    }
+    format_input
+}
+
+
 
 fn main() {
-    let mut horizontal_pos = 0;
-    let mut depth = 0;
 
     let input = fs::read_to_string("input")
         .expect("Couldn't read file");
 
-    let input1: Vec<&str> = input
-        .trim()
-        .lines()
-        .collect();
+    let format_input = format_input(&input);
 
-    let mut input2: Vec<Vec<&str>> = vec!();
-    for i in input1 {
+    let mut sub1 =  Sub::new();
+    for i in &format_input {
 
-        input2.push(i.split(' ').collect());
-    }
-    let mut input3 = vec!();
-    for i in input2.iter() {
-        let tup: (&str, i32) = (i[0], i[1].parse().expect("not a number"));
-        input3.push(tup);
+        sub1.part1(*i);
     }
 
-    for i in &input3 {
-        change_position(&mut horizontal_pos, &mut depth, &i);
+    let mut sub2 =  Sub::new();
+    for i in &format_input {
+
+        sub2.part2(*i);
     }
 
-    println!("part one: {}", horizontal_pos*depth);
+    println!("{}", sub1.horizontal_pos * sub1.depth);
+    println!("{}", sub2.horizontal_pos * sub2.depth);
 
-    let mut aim = 0;
-    depth = 0;
-    horizontal_pos = 0;
-
-    for i in input3 {
-        change_position_with_aim(&mut aim, &mut horizontal_pos, &mut depth, &i);
-    }
-    println!("part one: {}", horizontal_pos*depth);
-
-}
-
-fn change_position ( horizontal_pos: &mut i32, depth: &mut i32, command: &(&str, i32)) {
-    match command {
-        ("forward", val) => { *horizontal_pos += val }
-        ("down", val) => { *depth += val }
-        ("up", val) => { *depth -= val }
-        _ => panic!("Unexpected command")
-    }
-}
-
-fn change_position_with_aim (aim: &mut i32, horizontal_pos: &mut i32, depth: &mut i32, &command: &(&str, i32)) {
-    match command {
-        ("forward", val) => { *horizontal_pos += val; *depth += val * *aim; }
-        ("down", val) => { *aim += val; }
-        ("up", val) => { *aim -= val; }
-        _ => panic!("Unexpected command")
-    }
 }
