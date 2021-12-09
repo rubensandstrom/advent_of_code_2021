538fb058ac35b57d47e87a6d96bc4cef373ec66a
rubensandstrom@gmail.com <rubensandstrom@gmail.com>
2021-12-09 02:46:44 +0100

    input added

---
+++ day8/target/debug/day8.d
@@ -0,0 +1,1 @@
+/Users/rubensandstrom/Documents/Programmering/Rust/advent_of_code_2021/day8/target/debug/day8: /Users/rubensandstrom/Documents/Programmering/Rust/advent_of_code_2021/day8/src/main.rs
---
+++ day8/target/debug/deps/day8-a4814bf09145e24c.d
@@ -0,0 +1,5 @@
+/Users/rubensandstrom/Documents/Programmering/Rust/advent_of_code_2021/day8/target/debug/deps/day8-a4814bf09145e24c: src/main.rs
+
+/Users/rubensandstrom/Documents/Programmering/Rust/advent_of_code_2021/day8/target/debug/deps/day8-a4814bf09145e24c.d: src/main.rs
+
+src/main.rs:
