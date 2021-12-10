288189568010cb56b6c5f11525c957169f38c1c7
rubensandstrom@gmail.com <rubensandstrom@gmail.com>
2021-12-11 00:25:22 +0100

    messy but works

---
+++ day10/target/debug/day10.d
@@ -0,0 +1,1 @@
+/Users/rubensandstrom/Documents/Programmering/Rust/advent_of_code_2021/day10/target/debug/day10: /Users/rubensandstrom/Documents/Programmering/Rust/advent_of_code_2021/day10/src/main.rs
